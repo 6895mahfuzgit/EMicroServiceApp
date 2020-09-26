7c9e67ec0bf00a7c4d97e98312865b198c3663dc
6895mahfuzgit <6895mahfuz@gmail.com>
2020-09-26 18:52:17 +0600

    Get all Products API Implimented.

--- src/Catelog/Catelog.API/Controllers/CatelogController.cs
+++ src/Catelog/Catelog.API/Controllers/CatelogController.cs
@@ -2,14 +2,30 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Catelog.API.Entities;
+using Catelog.API.Repositories.Interfaces;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Catelog.API.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/v1/[controller]")]
     [ApiController]
     public class CatelogController : ControllerBase
     {
+        private readonly IProductRepository _productRepository;
+
+        public CatelogController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+        {
+            var products = await _productRepository.GetProducts();
+            return Ok(products);
+        }
+
     }
 }
