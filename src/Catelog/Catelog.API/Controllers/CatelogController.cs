48f43c0da46edc21de1abdd30549d4681a41b79e
6895mahfuzgit <6895mahfuz@gmail.com>
2020-09-27 11:44:00 +0600

    Implimented CRUD In APIs

--- src/Catelog/Catelog.API/Controllers/CatelogController.cs
+++ src/Catelog/Catelog.API/Controllers/CatelogController.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using Catelog.API.Entities;
 using Catelog.API.Repositories.Interfaces;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace Catelog.API.Controllers
 {
@@ -14,18 +16,71 @@ namespace Catelog.API.Controllers
     public class CatelogController : ControllerBase
     {
         private readonly IProductRepository _productRepository;
+        private readonly ILogger<CatelogController> _logger;
 
-        public CatelogController(IProductRepository productRepository)
+        public CatelogController(IProductRepository productRepository, ILogger<CatelogController> logger)
         {
             _productRepository = productRepository;
+            _logger = logger;
         }
 
         [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<Product>), (int)HttpStatusCode.OK)]
         public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
         {
             var products = await _productRepository.GetProducts();
             return Ok(products);
         }
+
+
+        [HttpGet("{id:length(24)}", Name = "GetProduct")]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        [ProducesResponseType(typeof(Product), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<Product>> GetProductById(string id)
+        {
+            var product = await _productRepository.GetProduct(id);
+            if (product == null)
+            {
+                _logger.LogError($"Product with id:{id}, not found");
+                return NotFound();
+            }
+            return Ok(product);
+        }
+
+        [Route("[action]/{category}")]
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<Product>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProductByCategory(string category)
+        {
+            var products = await _productRepository.GetProductByCategory(category);
+            return Ok(products);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(Product), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<Product>> CreateProduct([FromBody] Product product)
+        {
+            await _productRepository.Create(product);
+            return CreatedAtRoute("GetProduct", new { id = product.Id }, product);
+        }
+
+
+        [HttpPut]
+        [ProducesResponseType(typeof(Product), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult> UpdateProduct([FromBody] Product product)
+        {
+            return Ok(await _productRepository.Update(product));
+        }
+
+
+        [HttpDelete("{id:length(24)}")]
+        [ProducesResponseType(typeof(Product), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> DeleteProductById(string id)
+        {
+            return Ok(await _productRepository.Delete(id));
+        }
+
+
 
     }
 }
--- src/Catelog/Catelog.API/Repositories/Implimentations/ProductRepository.cs
+++ src/Catelog/Catelog.API/Repositories/Implimentations/ProductRepository.cs
@@ -42,7 +42,7 @@ namespace Catelog.API.Repositories.Implimentations
 
         public async Task<IEnumerable<Product>> GetProductByCategory(string categoryName)
         {
-            FilterDefinition<Product> filter = Builders<Product>.Filter.ElemMatch(p => p.Category, categoryName);
+            FilterDefinition<Product> filter = Builders<Product>.Filter.Eq(p => p.Category, categoryName);
 
             return await _context.Products
                                  .Find(filter)
