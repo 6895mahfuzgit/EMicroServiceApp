12c8d24970665fb4dc1e95869e6e3ca44ca6cf3e
6895mahfuzgit <6895mahfuz@gmail.com>
2020-10-26 20:38:53 +0600

    Added DI for MediateR,AutoMapper and Services.API Created For Order Fetch and Add . CQRS Pattern Completed.

--- src/Ordering/Ordering.API/Controllers/OrderController.cs
+++ src/Ordering/Ordering.API/Controllers/OrderController.cs
@@ -1,9 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
+using System.Net;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Ordering.Application.Commands;
+using Ordering.Application.Queries;
+using Ordering.Application.Responses;
 
 namespace Ordering.API.Controllers
 {
@@ -11,6 +13,32 @@ namespace Ordering.API.Controllers
     [ApiController]
     public class OrderController : ControllerBase
     {
+
+        private readonly IMediator _mediator;
+
+        public OrderController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<OrderResponse>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<IEnumerable<OrderResponse>>> GetOrderByUserName(string userName)
+        {
+            var query = new GetOrderByUserNameQuery(userName);
+            var orderList = await _mediator.Send(query);
+            return Ok(orderList);
+        }
+
+
+        [HttpPost]
+        [ProducesResponseType(typeof(OrderResponse), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<OrderResponse>> CheckoutOrder([FromBody] CheckoutOrderCommand command)
+        {
+            var result = await _mediator.Send(command);
+            return Ok(result);
+        }
 
     }
 }
