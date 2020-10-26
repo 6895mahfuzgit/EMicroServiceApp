87484243bf500b730fa02075b0efaad5ce88e345
6895mahfuzgit <6895mahfuz@gmail.com>
2020-10-26 13:28:47 +0600

    Added SQL Server connection to Order Microservice Project

--- src/Ordering/Ordering.API/Program.cs
+++ src/Ordering/Ordering.API/Program.cs
@@ -4,8 +4,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Ordering.Infrastructure.Data;
 
 namespace Ordering.API
 {
@@ -13,7 +15,9 @@ namespace Ordering.API
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            CreateAndSeedData(host);
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -22,5 +26,29 @@ namespace Ordering.API
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+
+        private static void CreateAndSeedData(IHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var logFactory = services.GetRequiredService<ILoggerFactory>();
+                try
+                {
+                    var orderContext = services.GetRequiredService<OrderContext>();
+                    OrderContextSeed.SeedAsync(orderContext, logFactory);
+                }
+                catch (Exception ex)
+                {
+                    var log = logFactory.CreateLogger<Program>();
+
+                    log.LogError(ex.Message);
+
+                }
+            }
+        }
+
+
+
     }
 }
