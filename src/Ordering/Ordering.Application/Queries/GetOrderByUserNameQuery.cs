b29c2707506de3249195241244e9d630b6dc8c97
6895mahfuzgit <6895mahfuz@gmail.com>
2020-10-26 16:29:08 +0600

    GetOrderByUserName Query,Response and Handler Added.

--- src/Ordering/Ordering.Application/Queries/GetOrderByUserNameQuery.cs
+++ src/Ordering/Ordering.Application/Queries/GetOrderByUserNameQuery.cs
@@ -1,10 +1,16 @@
-﻿using System;
+﻿using MediatR;
+using Ordering.Application.Responses;
 using System.Collections.Generic;
-using System.Text;
 
 namespace Ordering.Application.Queries
 {
-    public class GetOrderByUserNameQuery
+    public class GetOrderByUserNameQuery : IRequest<IEnumerable<OrderResponse>>
     {
+        public string  UserName { get; set; }
+
+        public GetOrderByUserNameQuery(string userName)
+        {
+            UserName = userName;
+        }
     }
 }
